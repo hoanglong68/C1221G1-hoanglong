8362a92ff4200cd7fd0e9463cd25dd44a9aa333e
Hoang Long <longhoang6668@gmail.com>
2022-01-07 17:03:57 +0700

    up bai

--- ss7_bien_du_lieu_toan_tu/thuc_hanh/SuDungToanTu.js
+++ ss7_bien_du_lieu_toan_tu/thuc_hanh/SuDungToanTu.js
@@ -1,8 +1,8 @@
-let inputwidth;
-let inputheight;
-inputwidth = prompt("enter the width")
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("enter the width")
 inputheight = prompt("enter the height")
-let width = parseInt(inputwidth)
-let height = parseInt(inputheight)
+let width = parseInt(inputWidth)
+let height = parseInt(inputHeight)
 let area = width * height;
 document.write('the area is: ' + area)
